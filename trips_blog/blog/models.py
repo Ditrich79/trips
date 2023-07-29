3f30a7c7e724e5c99a67b57715319d1dab4aed5b
Ditrich79 <adk18@mail.ru>
2023-07-29 20:58:06 +0300

    trips_29.07.23

---
+++ trips_blog/blog/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=255, unique=True, verbose_name='URL')
+    content = models.TextField(blank=True, verbose_name='Содержание')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Фотография')
+    time_created = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    picture1 = models.ImageField(upload_to='pictures/%Y/%m/%d/', blank=True, verbose_name='Картинка1')
+    picture2 = models.ImageField(upload_to='pictures/%Y/%m/%d/', blank=True, verbose_name='Картинка2')
+    picture3 = models.ImageField(upload_to='pictures/%Y/%m/%d/', blank=True, verbose_name='Картинка3')
+    picture4 = models.ImageField(upload_to='pictures/%Y/%m/%d/', blank=True, verbose_name='Картинка4')
+    picture5 = models.ImageField(upload_to='pictures/%Y/%m/%d/', blank=True, verbose_name='Картинка5')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'статью'
+        verbose_name_plural = 'Статьи'
+        ordering = ['-time_created']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    slug = models.SlugField(max_length=150, unique=True, verbose_name='URL')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'категорию'
+        verbose_name_plural = 'Категории'
