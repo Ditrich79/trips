8b7a341a16a1ae3f4e6babf18d4ae66e9bee28c8
Ditrich79 <adk18@mail.ru>
2023-08-02 21:42:43 +0300

    trips_02.08.23

--- trips_blog/blog/forms.py
+++ trips_blog/blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import *
+from django.core.exceptions import ValidationError
 
 
 class AddPostForm(forms.ModelForm):
@@ -7,3 +8,19 @@ class AddPostForm(forms.ModelForm):
         model = Blog
         fields = ['title', 'slug', 'content', 'photo', 'is_published', 'cat', 'picture1', 'picture2', 'picture3',
                   'picture4', 'picture5']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'slug': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10, 'class': 'form-input'})
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cat'].empty_label = 'Категория не выбрана'
+
+    def clean_title(self):
+        title = self.cleaned_data('title')
+        if len(title) > 100:
+            raise ValidationError('Длина заголовка превышает 100 символов')
+        return title
+    
--- trips_blog/blog/views.py
+++ trips_blog/blog/views.py
@@ -3,31 +3,25 @@ from .models import *
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 from .forms import *
+from .utils import *
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-menu = [
-    {'title': 'Добавить статью', 'url_name': 'add_article'},
-    {'title': 'Войти', 'url_name': 'index'},
-]
-
-
-class BlogHome(ListView):
+class BlogHome(DataMixin, ListView):
     model = Blog
     template_name = 'blog/index.html'
     context_object_name = 'posts'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Любитель путешествий'
-        context['cat_selected'] = 0
-        context['menu'] = menu
-        return context
+        c_def = self.get_user_context(title='Любитель путешествий')
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Blog.objects.filter(is_published=True).select_related('cat')
 
 
-class ShowArticle(DetailView):
+class ShowArticle(DataMixin, DetailView):
     model = Blog
     template_name = 'blog/article.html'
     context_object_name = 'article'
@@ -35,12 +29,11 @@ class ShowArticle(DetailView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = context['article']
-        context['menu'] = menu
-        return context
+        c_def = self.get_user_context(title=context['article'])
+        return dict(list(context.items()) + list(c_def.items()))
 
 
-class BlogCategory(ListView):
+class BlogCategory(DataMixin, ListView):
     model = Blog
     template_name = 'blog/index.html'
     context_object_name = 'posts'
@@ -51,19 +44,18 @@ class BlogCategory(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
-        context['menu'] = menu
-        context['cat_selected'] = context['posts'][0].cat_id
-        return context
+        c = Category.objects.get(slug=self.kwargs['cat_slug'])
+        c_def = self.get_user_context(title='Категория - ' + str(c.name), cat_selected=c.pk)
+        return dict(list(context.items()) + list(c_def.items()))
 
 
-class AddArticle(CreateView):
+class AddArticle(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'blog/addarticle.html'
     success_url = reverse_lazy('index')
+    login_url = reverse_lazy('index')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Добавление статьи'
-        context['menu'] = menu
-        return context
+        c_def = self.get_user_context(title='Добавление статьи')
+        return dict(list(context.items()) + list(c_def.items()))
