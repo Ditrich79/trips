67821747a31cf3e95abcb325baabe9350a74696c
Ditrich79 <adk18@mail.ru>
2023-08-12 21:18:58 +0300

    trips_12.08.23

--- trips_blog/blog/admin.py
+++ trips_blog/blog/admin.py
@@ -1,14 +1,32 @@
 from django.contrib import admin
 from .models import *
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
+from django.utils.safestring import mark_safe
+
+
+class ArticleAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Blog
+        fields = '__all__'
 
 
 class BlogAdmin(admin.ModelAdmin):
+    form = ArticleAdminForm
     prepopulated_fields = {'slug': ('title',)}
-    list_display = ('id', 'title', 'cat', 'time_created', 'photo', 'is_published')
+    list_display = ('id', 'title', 'cat', 'time_created', 'get_html_photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_created')
+
+    def get_html_photo(self, item):
+        if item.photo:
+            return mark_safe(f"<img src='{item.photo.url}' width='60'>")
+
+    get_html_photo.short_description = 'Фотография'
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -20,3 +38,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Blog, BlogAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_header = 'Административная панель сайта "Любитель путешествий"'
+admin.site.site_title = 'Административная панель сайта "Любитель путешествий"'
